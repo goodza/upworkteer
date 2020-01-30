991d714406fc54118a0b835bb808438152b6f5fb
gudza <henryclash@gmail.com>
2020-01-31 00:49:11 +0300

    work with Cookies

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const app = express();
 const puppeteer = require('puppeteer');
 const port = process.env.PORT || 8080;
 const validUrl = require('valid-url');
-const urlToScreenshot = 'http://github.com'
+const urlToScreenshot = 'https://www.upwork.com/ab/jobs/search/t/1/?ontology_skill_uid=1031626755474440192&sort=recency'
+const fs = require('fs').promises;
 
 console.log('HEADLESS MODE = '+process.env.HEADLESS)
 
@@ -23,16 +24,31 @@ const nonHeadFlag = {
     headless: false
 };
 
+const viewPort = { width: 1024, height: 768 };
+
+let SaveCookies = async (page) =>{
+	const cookies = await page.cookies();
+	await fs.writeFile('./cookies.json', JSON.stringify(cookies, null, 2));
+}
+
+let LoadCookies = async (page) =>{
+	const cookiesString = await fs.readFile('./cookies.json');
+	const cookies = JSON.parse(cookiesString);
+	await page.setCookie(...cookies);
+}
+
 (async () => {
     
     browser = await puppeteer.launch(Object.assign({
         args: ['--no-sandbox', '--disable-setuid-sandbox']},
-	{executablePath: 'google-chrome-stable'},
+	{executablePath: 'google-chrome-stable',
+	slowMo:10},
         process.env.HEADLESS === 'false' ? nonHeadFlag : void null));
 
     page = await browser.newPage();
+    await page.setViewport(viewPort);
+    await LoadCookies(page);
     await page.goto(urlToScreenshot);
-    
   })();
 
 
@@ -48,7 +64,7 @@ app.get('/ya', function(req, res) {
             res.send(buffer)
         });
 
-        await browser.close();
+      //  await browser.close();
     })();
     // res.send(urlToScreenshot)
 
